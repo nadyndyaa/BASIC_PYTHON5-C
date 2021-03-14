dcaae48de9b42cc1c3467ca94f77f0cc78c3aadf
nadyndyaa <80561549+nadyndyaa@users.noreply.github.com>
2021-03-14 17:38:42 +0700

    Add files via upload

---
+++ Tugas-1/soal-[2].py
@@ -0,0 +1,6 @@
+phi = 22/7
+r = int(input ("Masukkan jari-jari lingkaran : "))
+luas =phi * r * r
+print("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2.".format(r,luas))
+#format luas menjadi 2 angka dibelakang koma
+print("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2.".format(r,luas))
